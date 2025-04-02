1207f21e4d0fb0eb5785473d9fd1d22872ecc4ee
Andres M <marulandaandres19@gmail.com>
2025-04-02 12:16:41 -0500

    Implement movie fetching with loading and error handling in Index component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
-import { FlatList, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import SearchBar from '@/components/SearchBar';
 import { useRouter } from 'expo-router';
-import PopularMovies from '@/components/PopularMovies';
+import useFetch from '@/services/useFetch';
+import { fetchMovies } from '@/services/api';
+import MovieCard from '@/components/MovieCard';
 
 export default function Index() {
   const router = useRouter();
+
+  const { data, loading, error } = useFetch(() => fetchMovies({ query: '' }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -20,12 +24,39 @@ export default function Index() {
             placeholder="Search for a movie"
           />
         </View>
-
-        <View className="w-full mt-8 bg-slate-400 px-5 h-1/3">
-          <Text className="text-white">Popular Movies</Text>
-          <PopularMovies />
-        </View>
       </LinearGradient>
+      <>
+        <Text className=" text-left ml-3 mt-5 mb-3 text-lg font-bold text-white">
+          Popular Movies
+        </Text>
+      </>
+      <View>
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#FFD700"
+            className="mt-10 self-center"
+          />
+        ) : error ? (
+          <Text>Error: {error.message}</Text>
+        ) : (
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <MovieCard {...item} />}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 10 }}
+            className=" pt-2  "
+          />
+        )}
+      </View>
+
+      <>
+        <Text className=" text-left ml-3 mt-5 mb-3 text-xl font-bold text-white">
+          Latest Movies
+        </Text>
+      </>
     </View>
   );
 }
